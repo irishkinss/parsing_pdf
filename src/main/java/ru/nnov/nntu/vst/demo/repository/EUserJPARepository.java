ecdff7b762a09e995743c6395324935f4e88bdc3
irishkinss <irinashapkina.03@inbox.ru>
2025-01-12 15:35:08 +0300

    с парсером и сущностями

--- src/main/java/ru/nnov/nntu/vst/demo/repository/EUserJPARepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nnov.nntu.vst.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nnov.nntu.vst.demo.models.EUser;
-
-public interface EUserJPARepository extends JpaRepository<EUser, Long> {
-}
